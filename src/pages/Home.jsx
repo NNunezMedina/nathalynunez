5fe51f8e1f8be8742011a9f68ebd780677448cc8
Nathaly <nathalynunezmedina@gmail.com>
2025-11-29 12:17:44 -0500

    "add background style to homepage"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,19 @@ import "./Home.css";
 export default function Home() {
   return (
     <section id="home" className="home-container">
-
-      {/* Columna izquierda: tu nombre y descripción */}
       <div className="home-left">
         <p className="home-name">NATHALY NÚÑEZ</p>
         <p className="home-role">
           Shopify Developer | Front-End Web Developer
         </p>
         <p className="home-description">
-          Shopify Front-End Developer specialized in Liquid, React, and Tailwind CSS. I build scalable, high-performance eCommerce experiences focused on clean architecture, accessibility, and modern SEO.
+          Shopify Front-End Developer specialized in Liquid, React, and
+          Tailwind CSS. I build scalable, high-performance eCommerce
+          experiences focused on clean architecture, accessibility, and modern
+          SEO.
         </p>
       </div>
 
-      {/* Columna derecha: textos grandes */}
       <div className="home-right">
         <nav className="home-menu">
           <a href="#work">WORK</a>
@@ -23,7 +23,6 @@ export default function Home() {
           <a href="#contact">CONTACT</a>
         </nav>
       </div>
-
     </section>
   );
 }
