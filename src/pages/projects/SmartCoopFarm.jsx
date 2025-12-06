7f4fe1ed46b2d2a10cc80bb5b505816b367f7c2c
Nathaly <nathalynunezmedina@gmail.com>
2025-12-06 11:37:09 -0500

    "add smartcoopfamr page complete"

--- src/pages/projects/SmartCoopFarm.jsx
+++ src/pages/projects/SmartCoopFarm.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import "./SmartCoopFarm.css";
 import smart1 from "../../assets/smart1.png";
+import smart2 from "../../assets/smart2.png";
+import smart3 from "../../assets/smart3.png";
 
 export default function SmartCoopFarm() {
   useEffect(() => {
@@ -32,7 +34,7 @@ export default function SmartCoopFarm() {
       </section>
 
       {/* SECCIÓN DESCRIPTIVA */}
-      <section className="project-description page-wrapper reveal-on-scroll">
+      <section className="project-description page-wrapper ">
         <p className="project-intro">
           A Shopify e-commerce built for Smart Coop Farm, focused on 
           clean architecture, custom theme development and scalable performance.
@@ -81,6 +83,42 @@ export default function SmartCoopFarm() {
             apps or complicated configurations.
           </p>
         </section>
+        <section className="project-image-full reveal-on-scroll">
+          <img src={smart2} alt="Smart Coop Farm - Second Screenshot" />
+        </section>
+        <section className="project-image-full reveal-on-scroll">
+          <img src={smart3} alt="Smart Coop Farm - Second Screenshot" />
+        </section>
+
+        <section className="project-big-text reveal-on-scroll">
+        <h2 className="big-quote">
+          I really enjoy being able to define the content management system
+          structure of a website, systems are just my thing.
+        </h2>
+
+        <p className="small-description">
+          Paul was also so willing to do what was needed for the content, which was
+          a dream to work with. He created 10-second clips of his best videos in no
+          time and uploaded them to Vimeo for the home page carousel with
+          auto-playing video backgrounds. His amazing content really made this
+          website what it is.
+        </p>
+      </section>
+      <section className="project-video-full reveal-on-scroll">
+        <video
+          src="/smartvideo1.mp4"
+          autoPlay
+          muted
+          loop
+          playsInline
+          controls
+        />
+      </section>
+      <section className="project-button-section reveal-on-scroll">
+        <a href="https://smart.coop.farm" target="_blank" className="project-button">
+          Check it out →
+        </a>
+      </section>
       </section>
     </>
   );
